97b2dea06b17785183253d76ed279a93cb4a3ffa
Juan A <juan@Juans-MacBook-Air.local>
2024-03-27 14:07:38 -0400

    Part 1 adjustment - remove id from the card, doesnt seem needed

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -7,7 +7,6 @@ interface ProductViewProps {
 function ProductView({ product } : ProductViewProps) {
     return (
         <>
-            <p>{product.product_id}</p>
             <p>{product.title}</p>
             <img src={product.image}/>
         </>
